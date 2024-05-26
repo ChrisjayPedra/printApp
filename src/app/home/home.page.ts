43f78f767c63cbce46ebc65390078386c91feb7f
ChrisjayPedra <chrisjay.pedra.xentra@gmail.com>
2024-05-27 00:42:35 +0800

    commit app print

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { printPlugin } from 'print-plugin';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  async testPluginMethod(msg: any) {
+    await printPlugin.testPluginMethod({ msg: msg }).then((res: any) => {
+      alert('return value is ' + JSON.stringify(res.value));
+    }).catch((err:any)=>{
+      alert('err value is ' + JSON.stringify(err.value));
+    });
+  }
 }
